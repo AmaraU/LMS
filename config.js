ec8675ddfed36e46b13b932608ba1bc0b388e5f4
AmaraU <amaraude01@gmail.com>
2026-01-30 11:00:14 +0100

    library and new log in

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-export const BASE_URL = 'http://localhost:4090'
-// export const BASE_URL = 'https://lms-academy.onrender.com'
+// export const BASE_URL = 'http://localhost:4090'
+export const BASE_URL = 'https://lms-academy.onrender.com'
 export const TEST_URL = 'http://localhost:4090'
 export const ACADEMY_URL = 'https://academy.cwg-plc.com/Academy_Onboarding/Admission_Academy_Payment/Registeruser.aspx'
